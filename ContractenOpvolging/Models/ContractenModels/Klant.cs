883c3dc0d62b94f3d04dfa5b83e51f3f91ad5bae
Tim Vercauteren <Tim.Vercauteren@Kapture.be>
2017-05-08 12:51:12 +0200

    adding models to empty template project

---
+++ ContractenOpvolging/Models/ContractenModels/Klant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContractenOpvolging.Models.ContractenModels
+{
+    [Table("Klanten")]
+    public class Klant
+    {
+        [Key]
+        public int KlantID { get; set; }
+        [Required]
+        public string Naam { get; set; }
+        public string Contactpersoon { get; set; }
+        public string Telefoon { get; set; }
+        public string Email { get; set; }
+
+        //public Adres Adres { get; set; } to-Do add Complex Type Adres
+
+    }
+}
