efa42d0ca5558e3151d72d0049ab30bdec78c78e
Tim Vercauteren <Tim.Vercauteren@Kapture.be>
2017-05-08 13:29:14 +0200

    Adres maken en toevoegen aan klant

---
+++ ContractenOpvolging/Models/ContractenModels/Adres.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ContractenOpvolging.Models.ContractenModels
+{
+    [ComplexType]
+    public class Adres
+    {
+        [Column("Straat")]
+        public string Straat { get; set; }
+        [Column("HuisNr")]
+        public string HuisNr { get; set; }
+        [Column("PostCode")]
+        public string Postcode { get; set; }
+        [Column("Gemeente")]
+        public string Gemeente { get; set; }
+    }
+}
--- ContractenOpvolging/Models/ContractenModels/Klant.cs
+++ ContractenOpvolging/Models/ContractenModels/Klant.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using ContractenOpvolging.Models.ContractenModels;
 
 namespace ContractenOpvolging.Models.ContractenModels
 {
@@ -17,9 +18,7 @@ namespace ContractenOpvolging.Models.ContractenModels
         public string Contactpersoon { get; set; }
         public string Telefoon { get; set; }
         public string Email { get; set; }
-        //public Adres Adres { get; set; } to-Do add Complex Type Adres
-
-        //test voor nieuwe branch
+        public Adres Adres { get; set; } 
 
     }
 }
