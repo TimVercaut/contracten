282dcddbac5db19cac2ddd130a68f9e8f6faad9d
Tim Vercauteren <Tim.Vercauteren@Kapture.be>
2017-05-09 10:23:12 +0200

    Views verder aanpassen

---
+++ ContractenOpvolging/Data/Migrations/20170509081252_aanpassennullables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContractenOpvolging.Data.Migrations
+{
+    public partial class aanpassennullables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Tarief",
+                table: "Contracten",
+                nullable: true,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Tarief",
+                table: "Contracten",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+        }
+    }
+}
